e6e61da541c751f1f2ac2af266c061f09fdd75d5
Eugene Bulgakov <zoom81@yandex.ru>
2025-06-27 00:38:59 +0400

    Update eval cases

--- evals/experiments/itemsSearch.eval.js
+++ evals/experiments/itemsSearch.eval.js
@@ -12,6 +12,27 @@ Eval("ItemsSearch", {
         tools: [itemsSearchToolDefinition]
       }),
       expected: createToolCallMessage(itemsSearchToolDefinition.name)
+    },
+    {
+      input: runLLM({
+        messages: [{ role: "user", content: "Найди товар до 1000 рублей" }],
+        tools: [itemsSearchToolDefinition]
+      }),
+      expected: createToolCallMessage(itemsSearchToolDefinition.name)
+    },
+    {
+      input: runLLM({
+        messages: [{ role: "user", content: "Покажи самый дорогой товар" }],
+        tools: [itemsSearchToolDefinition]
+      }),
+      expected: createToolCallMessage(itemsSearchToolDefinition.name)
+    },
+    {
+      input: runLLM({
+        messages: [{ role: "user", content: "Найди все мышки в продаже" }],
+        tools: [itemsSearchToolDefinition]
+      }),
+      expected: createToolCallMessage(itemsSearchToolDefinition.name)
     }
   ],
   task: () => itemsSearchToolDefinition.name,
